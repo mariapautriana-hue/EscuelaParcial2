8ba611843adfdb15869b396f2c16a4311d418c74
MARIA PAULA TRIANA DAZA <maria_pau.triana@uao.edu.co>
2026-04-13 11:19:58 -0500

    primer commit

---
+++ src/main/java/com/mycompany/escuek/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escuek;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
